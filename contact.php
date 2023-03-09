ad5c83ecd2ccc93b0976cd86147e4c8b6b42c2cc
Isaac Phoon Nicholas <ikp25@drexel.edu>
2023-03-09 18:56:51 +0000

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,27 +1,15 @@
-
-<link rel="stylesheet" href="data.css">
 <?php
-
-   
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $data = fopen("data/data.txt","a") or die("Cannot open the file");
-    fwrite($data, "$fname, $lname, $email,  $subject\n");
-    echo "<h1>The data has been submitted</h1>";
-    fclose($data);
-
-    $conn = new mysqli('localhost', 'root','','test')
-    if($conn->connect_error){
-        die('Connection Failed:'.$conn->connect_error);
-    }else{
-        $stmt = $conn->preprare("insert into registration(fname, lname, email, subject)
-            value(?, ?, ?, ?, ?, ?) ");
-        $stmt->bind_param("sssiss", $Fname, $Lname, $email, $subject;
-        $stmt->execute();
-        echo "registration Successful...";
-        $stmt->close();
-        $conn->close();
+if(isset($_POST['submit'])){
+    $to = "ikp25@drexel.edu";
+    $subject = $_POST['subject'];
+    $message = "Name: ".$_POST['firstname']." ".$_POST['lastname']."\r\n";
+    $message .= "Email: ".$_POST['email']."\r\n";
+    $message .= "Message: ".$_POST['subject']."\r\n";
+    $headers = "From: ".$_POST['email']."\r\n";
+    if(mail($to, $subject, $message, $headers)){
+        echo "Thank you for your message!";
+    } else {
+        echo "Sorry, there was a problem sending your message. Please try again later.";
     }
+}
 ?>
